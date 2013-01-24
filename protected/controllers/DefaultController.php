521dc47c99c11dc1435adf347ea1e557832cb647
Leon Zhuang <leon@nerrsoft.com>
2013-01-24 16:57:19 +0800

    recode login action

--- protected/controllers/DefaultController.php
+++ protected/controllers/DefaultController.php
@@ -87,25 +87,12 @@ class DefaultController extends Controller
 	 */
 	public function actionLogin()
 	{
-		$model=new LoginForm;
-
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
+		if(!Yii::app()->user->isGuest)
+			$this->redirect(Yii::app()->homeUrl);
+		$formSubmitUrl = $this->createUrl('default/auth');
+		$sucessUrl = Yii::app()->user->returnUrl;
+		// display the login page
+		$this->renderPartial('login',array('formSubmitUrl'=>$formSubmitUrl, 'sucessUrl'=>$sucessUrl));
 	}
 
 	/**
