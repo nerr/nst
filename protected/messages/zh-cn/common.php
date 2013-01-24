72e6dc0ffca6125b83e992db91c6fab0f0d4acde
Leon Zhuang <leon@nerrsoft.com>
2013-01-24 17:08:11 +0800

    add login page trans

--- protected/messages/zh-cn/common.php
+++ protected/messages/zh-cn/common.php
@@ -5,9 +5,16 @@ return array(
 
 
     // menu
-    'General'=>'摘要',
+    'General' => '摘要',
     'General Info' => '信息摘要',
     'General information and Summary' => '信息摘要和总结',
+    
+    
+    // login
+    'Login' => '登录',
+    'Login in' => '登录',
+    'User' => '用户',
+    'Pass' => '密码',
 );
 
 ?>
