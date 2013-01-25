e65b369e950d267b33ce3ca0e0529a300ef7a9b8
Leon Zhuang <leon@nerrsoft.com>
2013-01-25 14:00:32 +0800

    add index controller

---
+++ protected/controllers/IndexController.php
@@ -0,0 +1,44 @@
+<?php
+
+class IndexController extends Controller
+{
+	/**
+	 * Declares class-based actions.
+	 */
+	public function actions()
+	{
+		return array(
+			// captcha action renders the CAPTCHA image displayed on the contact page
+			'captcha'=>array(
+				'class'=>'CCaptchaAction',
+				'backColor'=>0xFFFFFF,
+			),
+			// page action renders "static" pages stored under 'protected/views/site/pages'
+			// They can be accessed via: index.php?r=site/page&view=FileName
+			'page'=>array(
+				'class'=>'CViewAction',
+			),
+		);
+	}
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+		$this->redirect('index.php?r=default/general');
+	}
+
+	/**
+	 * This is the user general action
+	 */
+	public function actionGeneral()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+
+		$params['imgpath'] = Yii::app()->request->baseUrl."/themes/kanrisha/img/";
+		$this->render('general', $params);
+	}
+}
