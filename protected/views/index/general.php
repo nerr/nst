e025e439426d54db6e0fc1ac453bb4b31783bea1
@nerrsoft <leon@nerrsoft.com>
2013-02-18 12:55:52 +0800

    add mark menu auto featrue

--- protected/views/index/general.php
+++ protected/views/index/general.php
@@ -1,54 +1,8 @@
-<div class="wrapper small_menu">
-		<ul class="menu_small_buttons">
-			<li><a title="General Info" class="i_22_dashboard smActive" href="index.html"></a></li>
-			<li><a title="Your Messages" class="i_22_inbox" href="inbox.html"></a></li>
-			<li><a title="Visual Data" class="i_22_charts" href="charts.html"></a></li>
-			<li><a title="Kit elements" class="i_22_ui" href="ui.html"></a></li>
-			<li><a title="Some Rows" class="i_22_tables" href="tables.html"></a></li>
-			<li><a title="Some Fields" class="i_22_forms" href="forms.html"></a></li>
-		</ul>
-	</div>
-
 	<div class="wrapper contents_wrapper">
 		
 		<aside class="sidebar">
 			<ul class="tab_nav">
-				<li class="active_tab i_32_dashboard">
-					<a href="index.html" title="<?php echo Yii::t('common', 'General Info'); ?>">
-						<span class="tab_label"><?php echo Yii::t('common', 'General'); ?></span>
-						<span class="tab_info"><?php echo Yii::t('common', 'General Info'); ?></span>
-					</a>
-				</li>
-				<li class="i_32_inbox">
-					<a href="inbox.html" title="Your Messages">
-						<span class="tab_label">Inbox</span>
-						<span class="tab_info">Your Messages</span>
-					</a>
-				</li>
-				<li class="i_32_charts">
-					<a href="charts.html" title="Visual Data">
-						<span class="tab_label">Charts</span>
-						<span class="tab_info">Visual Data</span>
-					</a>
-				</li>
-				<li class="i_32_ui">
-					<a href="ui.html" title="Kit elements">
-						<span class="tab_label">UI</span>
-						<span class="tab_info">Kit elements</span>
-					</a>
-				</li>
-				<li class="i_32_tables">
-					<a href="tables.html" title="Some Rows">
-						<span class="tab_label">Report</span>
-						<span class="tab_info">Some Rows</span>
-					</a>
-				</li>
-				<li class="i_32_forms">
-					<a href="forms.html" title="Some Fields">
-						<span class="tab_label">Forms</span>
-						<span class="tab_info">Some Fields</span>
-					</a>
-				</li>
+				<?php echo $menu; ?>
 			</ul>
 		</aside>
 
@@ -64,30 +18,6 @@
 					<div class="simple_buttons">
 						<div class="bwIcon i_16_help"><?php echo Yii::t('common', 'Help'); ?></div>
 					</div>
-					<!-- <div class="simple_buttons">
-						<div class="bwIcon i_16_settings">Settings</div>
-						<div class="buttons_arrow"> -->
-							<!-- Drop Menu -->
-							<!-- <ul class="drop_menu menu_with_icons right_direction">
-								<li>
-									<a class="i_16_add" href="#" title="New Flie">
-										<span class="label">New File</span>
-									</a>
-								</li>
-								<li>
-									<a class="i_16_progress" href="#" title="2 Files Left">
-										<span class="label">Files Left</span>
-										<span class="small_count">2</span>
-									</a>
-								</li>
-								<li>
-									<a class="i_16_files" href="#" title="Browse Files">
-										<span class="label">Browse Files</span>
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div> -->
 				</div>
 
 				<div class="g_12 separator"><span></span></div>
