6b882e9c91a7c09db6577f11bdde05438c81ab7d
@nerrsoft <leon@nerrsoft.com>
2013-06-24 14:47:35 +0800

    create cron config

---
+++ protected/config/cron.php
@@ -0,0 +1,41 @@
+<?php
+
+return array(
+    // This path may be different. You can probably get it from `config/main.php`.
+    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+    'name'=>'NST Cron',
+ 
+    'preload'=>array('log'),
+ 
+    'import'=>array(
+        'application.components.*',
+        'application.models.*',
+    ),
+    // We'll log cron messages to the separate files
+    'components'=>array(
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'CFileLogRoute',
+                    'logFile'=>'cron.log',
+                    'levels'=>'error, warning',
+                ),
+                array(
+                    'class'=>'CFileLogRoute',
+                    'logFile'=>'cron_trace.log',
+                    'levels'=>'trace',
+                ),
+            ),
+        ),
+ 
+        // Your DB connection
+        'db'=>array(
+			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=nst',
+			'emulatePrepare' => true,
+			'username' => 'postgres',
+			'password' => '911911',
+			'charset' => 'utf8',
+		),
+    ),
+);
