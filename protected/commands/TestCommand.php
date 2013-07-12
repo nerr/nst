63b008bd2173e7839da3250d4f4835c79cbc879e
@nerrsoft <leon@nerrsoft.com>
2013-07-12 11:30:27 +0800

    add a test command use to test cron sms report task

---
+++ protected/commands/TestCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+class TestCommand extends CConsoleCommand
+{
+    public function run($args)
+    {
+        /*$criteria = new CDbCriteria;
+        $criteria->select = 'id,mobile';
+        $criteria->condition='id in (1,5)';
+        $result = SysUser::model()->findAll($criteria);*/
+
+        /*foreach($result as $val)
+        {
+            $data = Calculate::getGeneralSummaryData($val->id);
+
+            $msg = '截止'.$data['summary']['lastuptodate']."\r";
+            $msg .= '账户余额: '.number_format($data['summary']['balance'], 2)."\r";
+            $msg .= '交易成本: '.number_format($data['summary']['cost'], 2)."\r";
+            $msg .= '累计获得掉期: '.number_format($data['summary']['swap'], 2)."\r";
+            $msg .= '浮动收益: '.number_format($data['summary']['netearning'], 2);
+            $msg .= '[NST自动短信报表]';
+
+            //$m = echo mb_convert_encoding($msg, 'GBK', 'UTF-8');
+            echo $msg;
+            $this->sendSms($val->mobile, $msg);
+        }*/
+
+        $un = 5;
+        $data = Calculate::getGeneralSummaryData($un);
+
+        $msg = '截止'.$data['summary']['lastuptodate']."\r";
+        $msg .= '账户余额: '.number_format($data['summary']['balance'], 2)."\r";
+        $msg .= '交易成本: '.number_format($data['summary']['cost'], 2)."\r";
+        $msg .= '累计获得掉期: '.number_format($data['summary']['swap'], 2)."\r";
+        $msg .= '浮动收益: '.number_format($data['summary']['netearning'], 2);
+        $msg .= '[NST自动短信报表]';
+
+        //$m = echo mb_convert_encoding($msg, 'GBK', 'UTF-8');
+        echo $msg;
+        $this->sendSms('13524289996', $msg);
+    }
+
+    //-- send sms
+    protected function sendSms($mobile, $text)
+    {
+        $msg = urlencode($text);
+
+        $url  = 'http://210.5.158.31/hy/?expid=0&encode=utf8';
+        $url .= '&uid='.Yii::app()->params['smsAccount'];
+        $url .= '&auth='.Yii::app()->params['smsPassword'];
+        $url .= '&mobile='.$mobile;
+        $url .= '&msg='.$msg;
+        
+        try{
+
+            $ch = curl_init();
+            curl_setopt ($ch, CURLOPT_URL, $url);
+            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            $statuscode = curl_exec($ch);
+            curl_close($ch);
+
+            $code = explode(',', $statuscode);
+
+            if($code[0]==0)
+                //echo "Date:".date('Y-m-d H:i:s', time()).";Finished\n";
+                return true;
+            else
+                //echo "Date:".date('Y-m-d H:i:s', time()).";Failed\n";
+                return false;
+        }
+        catch( Exception $e)
+        {
+            //echo "Date:".date('Y-m-d H:i:s', time()).";ERROR:".$e."\n";
+            return false;
+        }
+    }
+}
