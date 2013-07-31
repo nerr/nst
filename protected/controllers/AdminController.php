d0efae84a21045123bfca6f1b769f57d4714e058
@nerrsoft <leon@nerrsoft.com>
2013-07-31 17:06:49 +0800

    finished admin user action

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -2,87 +2,84 @@
 
 class AdminController extends Controller
 {
-	public function filters()
-	{
-		return array(
-			'accessControl',
-		);
-	}
+    public function filters()
+    {
+        return array(
+            'accessControl',
+        );
+    }
 
-	public function accessRules()
-	{
-		return array(
-			array('allow',
-				'actions'=>array('index', 'general', 'swap', 'user'),
-				'users'=>array('leon@nerrsoft.com'),
-			),
-			array('deny',
-				'actions'=>array('index', 'general', 'report', 'funds','swap'),
-				'users'=>array('?','@'),
-			),
-		);
-	}
+    public function accessRules()
+    {
+        return array(
+            array('allow',
+                'actions'=>array('index', 'general', 'swap', 'user'),
+                'users'=>array('leon@nerrsoft.com'),
+            ),
+            array('deny',
+                'actions'=>array('index', 'general', 'report', 'funds','swap'),
+                'users'=>array('?','@'),
+            ),
+        );
+    }
 
-	/**
-	 * Declares class-based actions.
-	 */
-	public function actions()
-	{
-		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
-			),
-			// page action renders "static" pages stored under 'protected/views/site/pages'
-			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page'=>array(
-				'class'=>'CViewAction',
-			),
-		);
-	}
+    /**
+     * Declares class-based actions.
+     */
+    public function actions()
+    {
+        return array(
+            // captcha action renders the CAPTCHA image displayed on the contact page
+            'captcha'=>array(
+                'class'=>'CCaptchaAction',
+                'backColor'=>0xFFFFFF,
+            ),
+            // page action renders "static" pages stored under 'protected/views/site/pages'
+            // They can be accessed via: index.php?r=site/page&view=FileName
+            'page'=>array(
+                'class'=>'CViewAction',
+            ),
+        );
+    }
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
-	public function actionIndex()
-	{
-		$this->redirect('admin/general');
-	}
-
-
-	public function actionGeneral()
-	{
-		//var_dump(Yii::app()->user->gid);
-
-		$uid = Yii::app()->user->id;
-		$gid = Yii::app()->user->gid;
-
-		$params['menu'] = Menu::make($gid, 'General');
-		$this->render('general', $params);
-	}
-
-	public function actionUser()
-	{
-		//var_dump(Yii::app()->user->gid);
-
-		$uid = Yii::app()->user->id;
-		$gid = Yii::app()->user->gid;
-
-		$params['menu'] = Menu::make($gid, 'User');
-		$this->render('user', $params);
-	}
-
-	public function actionSwap()
-	{	
-		$uid = Yii::app()->user->id;
-		$gid = Yii::app()->user->gid;
-
-		$params['menu'] = Menu::make($gid, 'Swap');
-		$this->render('swap', $params);
+    /**
+     * This is the default 'index' action that is invoked
+     * when an action is not explicitly requested by users.
+     */
+    public function actionIndex()
+    {
+        $this->redirect('admin/general');
+    }
 
 
-		
-	}
+    public function actionGeneral()
+    {
+        $params['menu'] = Menu::make(Yii::app()->user->gid, 'General');
+        $this->render('general', $params);
+    }
+
+    public function actionUser()
+    {
+        //--  get menu data
+        $params['menu'] = Menu::make(Yii::app()->user->gid, 'User');
+        //-- get user list
+        $criteria = new CDbCriteria;
+        $criteria->order = 'id';
+        $userlist = ViewSysUserList::model()->findAll($criteria);
+        //-- get login log
+        $criteria = new CDbCriteria;
+        $criteria->order = 'logintime';
+        $loginlog = SysLoginLog::model()->findAll($criteria);
+
+        $params['loginlog'] = $loginlog;
+        $params['userlist'] = $userlist;
+
+        $this->render('user', $params);
+    }
+
+    public function actionSwap()
+    {   
+        $params['menu'] = Menu::make(Yii::app()->user->gid, 'Swap');
+        $this->render('swap', $params);
+    }
 }
