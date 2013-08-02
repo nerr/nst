64531d797dcee44de08333d9869b36c6648b09b5
@nerrsoft <leon@nerrsoft.com>
2013-08-02 16:18:11 +0800

    change for test

--- protected/commands/shell/EmailreportCommand.php
+++ protected/commands/shell/EmailreportCommand.php
@@ -6,13 +6,14 @@ class EmailreportCommand extends CConsoleCommand
     {
         $criteria = new CDbCriteria;
         $criteria->select = 'id,emaillist';
-        $criteria->condition='usergroupid=2 and id<>2';
+        //$criteria->condition='usergroupid=2 and id<>2';
+        $criteria->condition='id=9';
         $uid = SysUser::model()->findAll($criteria);
 
         if($uid)
         {
             $title = 'NST Weekly Report';
-            $sender = 'info@nerrsoft.com';
+            $body = '';
 
             foreach($uid as $u)
             {
@@ -23,16 +24,12 @@ class EmailreportCommand extends CConsoleCommand
 
                 if(!file_exists($attachment) continue; //-- if not exist behend create it that maybe some problem there.
 
-                $emailArr = $this->pregEmailAddress($u->emaillist);
+                //$to = $this->pregEmailAddress($u->emaillist);
+                $to = array('vickey@nerrsoft.com');
 
-                $this->sendEmail($title, $emailArr, $attachment, $sender);
+                Notification::nSendMail($title, $body, $to, array('leon@nerrsoft.com'), $attachment);
             }
         }
-    }
-
-    protected function sendEmail($title, $emailArr, $sender)
-    {
-        
     }
 
     protected function pregEmailAddress($str)
