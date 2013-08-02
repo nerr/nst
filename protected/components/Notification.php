0f2b986f5ce007f17e6d58674c5933b811159a30
@nerrsoft <leon@nerrsoft.com>
2013-08-02 11:44:31 +0800

    finished the Fetion send sms func

--- protected/components/Notification.php
+++ protected/components/Notification.php
@@ -1,36 +1,52 @@
 <?php
 
-class Notification()
+class Notification
 {
-	public static function nSmsfetion($mobile, $msg)
-	{
-		$fetion = new PHPFetion($this->_mobile, $this->_password);
-		try
-		{
-			$result = $fetion->send($mobile, $this->_smsTextPrefix.$msg.$this->_smsTextSuffix);
-			$count = 0;
-			while(!$this->checkStatus($result) && $count < $this->_trytime )
-			{
-				$count++;
-				sleep(2);
-				//echo "Sleep 2s, Re Send\n";
-				$result = $result = $fetion->send($mobile, $this->_smsTextPrefix.$msg.$this->_smsTextSuffix);
-			}
-			if ($count != ($this->_trytime - 1))
-			{
-				//echo "Date:".date('Y-m-d H:i:s', time()).";Finished\n";
-				return true;
-			}
-			else
-			{
-				//echo "Date:".date('Y-m-d H:i:s', time()).";Failed\n";
-				return false;
-			}
-		}
-		catch( Exception $e)
-		{
-			//echo "Date:".date('Y-m-d H:i:s', time()).";ERROR:".$e."\n";
-			return false;
-		}
-	}
+    public static function nSmsfetion($mobile, $msg, $trytime = 2)
+    {
+        $fAccount = Yii::app()->params->fetionAccount;
+        $fPassword = Yii::app()->params->fetionPassword;
+
+        Yii::import('ext.phpfetion.PHPFetion'); 
+        $fetion = new PHPFetion($fAccount, $fPassword);
+        try
+        {
+            $result = $fetion->send($mobile, $msg);
+            $count = 0;
+            while(!Notification::nCheckFetionStatus($result) && $count < $trytime )
+            {
+                $count++;
+                sleep(2);
+                //echo "Sleep 2s, Re Send\n";
+                $result = $fetion->send($mobile, $msg);
+            }
+            if($count != ($trytime - 1))
+            {
+                //echo "Date:".date('Y-m-d H:i:s', time()).";Finished\n";
+                return true;
+            }
+            else
+            {
+                //echo "Date:".date('Y-m-d H:i:s', time()).";Failed\n";
+                return false;
+            }
+        }
+        catch( Exception $e)
+        {
+            //echo "Date:".date('Y-m-d H:i:s', time()).";ERROR:".$e."\n";
+            return false;
+        }
+    }
+
+    public static function nCheckFetionStatus($status_code)
+    {
+        preg_match('/^.*HTTP\/1\.1 200 OK.*$/si', $status_code, $status);
+        if(isset($status[0]))
+            return true;
+        else
+        {
+            //echo "Result:$status_code\n";
+            return false;
+        }
+    }
 }
