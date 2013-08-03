9f53898ba0963e7f9be9a6052da8b8e93d39b8a9
@nerrsoft <leon@nerrsoft.com>
2013-08-03 08:33:29 +0800

    debug, can not work yet

--- protected/commands/EmailreportCommand.php
+++ protected/commands/EmailreportCommand.php
@@ -2,32 +2,37 @@
 
 class EmailreportCommand extends CConsoleCommand
 {
-    public function weekly()
+    public function run()
     {
         $criteria = new CDbCriteria;
-        $criteria->select = 'id,emaillist';
+        $criteria->select = 'id';
         //$criteria->condition='usergroupid=2 and id<>2';
         $criteria->condition='id=9';
         $uid = SysUser::model()->findAll($criteria);
 
         if($uid)
         {
-            $title = 'NST Weekly Report';
-            $body = '';
+            $title = 'NST Weekly Report '.date('Y-m-d');
+            $body = '这是系统自动发送的邮件，请勿回复。';
 
             foreach($uid as $u)
             {
-                $attachment = Yii::app()->params->xlsxPath.'NST.Weekly.'.date('Y-m-d').'-'.$uid;
+                
+                $attachment = Yii::app()->params->xlsxPath.'NST.Weekly.'.date('Y-m-d').'-'.$u->id.'.xlsx';
+
+                /*if(!file_exists($attachment)
+                    Excel::weekly($u->id, 'W');*/
 
                 if(!file_exists($attachment)
-                    Excel::weekly($u->id, 'W');
-
-                if(!file_exists($attachment) continue; //-- if not exist behend create it that maybe some problem there.
+                {
+                    echo 'no attachment file ';
+                    continue; //-- if not exist behend create it that maybe some problem there.
+                }
 
                 //$to = $this->pregEmailAddress($u->emaillist);
                 $to = array('vickey@nerrsoft.com');
 
-                Notification::nSendMail($title, $body, $to, array('leon@nerrsoft.com'), $attachment);
+                //Notification::nSendMail($title, $body, $to, array('leon@nerrsoft.com'), $attachment);
             }
         }
     }
