cdc6942628370e096e28e268f19846a117211fa4
@nerrsoft <leon@nerrsoft.com>
2013-08-05 16:47:04 +0800

    mv getClientIp() func to common classTools.php

--- protected/controllers/DefaultController.php
+++ protected/controllers/DefaultController.php
@@ -75,7 +75,7 @@ class DefaultController extends Controller
 	public function actionAuth()
 	{
 		$authproof = $_POST['proof'];
-		$authproof['ipaddress'] = $this->getClientIp();
+		$authproof['ipaddress'] = Tools::tGetClientIp();
 		$authproof['logintime'] = date('Y-m-d H:i:s', time());
 
 		$identity = new UserIdentity($authproof['email'], $authproof['password']);
@@ -106,19 +106,5 @@ class DefaultController extends Controller
 		$model = new SysLoginLog;
 		$model->attributes = $attr;
 		$model->save();
-	}
-
-	private function getClientIp()
-	{
-		if (isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP']))
-			return $_SERVER['HTTP_CLIENT_IP'];
-		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-			return strtok($_SERVER['HTTP_X_FORWARDED_FOR'], ',');
-		if (isset($_SERVER['HTTP_PROXY_USER']) && !empty($_SERVER['HTTP_PROXY_USER']))
-			return $_SERVER['HTTP_PROXY_USER'];
-		if (isset($_SERVER['REMOTE_ADDR']) && !empty($_SERVER['REMOTE_ADDR']))
-			return $_SERVER['REMOTE_ADDR'];
-		else
-			return "0.0.0.0";
 	}
 }
