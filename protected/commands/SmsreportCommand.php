e6533b191f2ad0c2e99412148ca6adfcbfc14e45
@nerrsoft <leon@nerrsoft.com>
2013-08-05 16:03:58 +0800

    add '收益率' item and change sms source

--- protected/commands/SmsreportCommand.php
+++ protected/commands/SmsreportCommand.php
@@ -18,47 +18,13 @@ class SmsreportCommand extends CConsoleCommand
             $msg .= '交易成本: '.number_format($data['summary']['cost'], 2)."\r";
             $msg .= '累计获得掉期: '.number_format($data['summary']['swap'], 2)."\r";
             $msg .= '浮动收益: '.number_format($data['summary']['netearning'], 2);
+            if(($data['summary']['balance'] - $data['summary']['netearning']) > 0)
+            $msg .= '收益率: '.number_format($data['summary']['netearning']/($data['summary']['balance'] - $data['summary']['netearning']), 2).'%';
             $msg .= '[NST自动短信报表]';
 
             //$m = echo mb_convert_encoding($msg, 'GBK', 'UTF-8');
-            echo $msg;
-            $this->sendSms($val->mobile, $msg);
-        }
-    }
 
-    //-- send sms
-    protected function sendSms($mobile, $text)
-    {
-        $msg = urlencode($text);
-
-        $url  = 'http://210.5.158.31/hy/?expid=0&encode=utf8';
-        $url .= '&uid='.Yii::app()->params['smsAccount'];
-        $url .= '&auth='.Yii::app()->params['smsPassword'];
-        $url .= '&mobile='.$mobile;
-        $url .= '&msg='.$msg;
-        
-        try{
-
-            $ch = curl_init();
-            curl_setopt ($ch, CURLOPT_URL, $url);
-            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
-            $statuscode = curl_exec($ch);
-            curl_close($ch);
-
-            $code = explode(',', $statuscode);
-
-            if($code[0]==0)
-                //echo "Date:".date('Y-m-d H:i:s', time()).";Finished\n";
-                return true;
-            else
-                //echo "Date:".date('Y-m-d H:i:s', time()).";Failed\n";
-                return false;
-        }
-        catch( Exception $e)
-        {
-            //echo "Date:".date('Y-m-d H:i:s', time()).";ERROR:".$e."\n";
-            return false;
+            Notification::nSendSms($val->mobile, $msg);
         }
     }
 }
