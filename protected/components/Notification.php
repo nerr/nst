d8f4137be99707378054e3c43d98ef24fc79a207
@nerrsoft <leon@nerrsoft.com>
2013-08-05 16:01:20 +0800

    add send sms func third part sms api

--- protected/components/Notification.php
+++ protected/components/Notification.php
@@ -2,6 +2,41 @@
 
 class Notification
 {
+    public static function nSendSms($mobile, $text)
+    {
+        $msg = urlencode($text);
+
+        $url  = 'http://210.5.158.31/hy/?expid=0&encode=utf8';
+        $url .= '&uid='.Yii::app()->params['smsAccount'];
+        $url .= '&auth='.Yii::app()->params['smsPassword'];
+        $url .= '&mobile='.$mobile;
+        $url .= '&msg='.$msg;
+        
+        try{
+
+            $ch = curl_init();
+            curl_setopt ($ch, CURLOPT_URL, $url);
+            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            $statuscode = curl_exec($ch);
+            curl_close($ch);
+
+            $code = explode(',', $statuscode);
+
+            if($code[0]==0)
+                //echo "Date:".date('Y-m-d H:i:s', time()).";Finished\n";
+                return true;
+            else
+                //echo "Date:".date('Y-m-d H:i:s', time()).";Failed\n";
+                return false;
+        }
+        catch( Exception $e)
+        {
+            //echo "Date:".date('Y-m-d H:i:s', time()).";ERROR:".$e."\n";
+            return false;
+        }
+    }
+
     public static function nSmsfetion($mobile, $msg, $trytime = 2)
     {
         $fAccount = Yii::app()->params->fetionAccount;
@@ -50,7 +85,7 @@ class Notification
         }
     }
 
-    public static function nSendMail($title, $body, $to, $cc = array(), $attach)
+    public static function nSendMail($title, $body, $to, $cc = array(), $attach = '')
     {
         if(count($to) == 0)
             return false;
@@ -69,7 +104,8 @@ class Notification
                     $mailer->AddCC($v);
             }
 
-            $mailer->AddAttachment($attach);
+            if(strlen($attach) > 0)
+                $mailer->AddAttachment($attach);
 
             return $mailer->send();
         }
