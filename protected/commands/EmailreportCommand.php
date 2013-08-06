17fef5dda5424c1bb3525775c2f5f2230e85ccc2
@nerrsoft <leon@nerrsoft.com>
2013-08-06 15:34:41 +0800

    test sending email report ok.

--- protected/commands/EmailreportCommand.php
+++ protected/commands/EmailreportCommand.php
@@ -17,22 +17,23 @@ class EmailreportCommand extends CConsoleCommand
 
             foreach($uid as $u)
             {
-                
-                $attachment = Yii::app()->params->xlsxPath.'NST.Weekly.'.date('Y-m-d').'-'.$u->id.'.xlsx';
+                $attachment = Yii::app()->params->xlsxPath.'NST.Weekly.'.date('Y-m-d').'.'.$u->id.'.xlsx';
 
-                /*if(!file_exists($attachment)
-                    Excel::weekly($u->id, 'W');*/
+                if(!file_exists($attachment))
+                    Excel::weekly($u->id, 'W');
 
-                if(!file_exists($attachment)
+                if(!file_exists($attachment))
                 {
-                    echo 'no attachment file ';
+                    echo 'no attachment file '."\r\n";
                     continue; //-- if not exist behend create it that maybe some problem there.
                 }
 
                 //$to = $this->pregEmailAddress($u->emaillist);
                 $to = array('vickey@nerrsoft.com');
 
-                //Notification::nSendMail($title, $body, $to, array('leon@nerrsoft.com'), $attachment);
+                $res = Notification::nSendMail($title, $body, $to, array('leon@nerrsoft.com'), $attachment);
+
+                var_dump($res);
             }
         }
     }
