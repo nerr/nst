f152d342d4efad7f7bd84f1974c573d2db40dd32
@nerrsoft <leon@nerrsoft.com>
2013-08-07 12:17:33 +0800

    add mobile number check before send sms

--- protected/commands/SmsreportCommand.php
+++ protected/commands/SmsreportCommand.php
@@ -24,7 +24,9 @@ class SmsreportCommand extends CConsoleCommand
 
             //$m = echo mb_convert_encoding($msg, 'GBK', 'UTF-8');
 
-            Notification::nSendSms($val->mobile, $msg);
+            if(count(Tools::tPregMobileNum($val->mobile)) > 0)
+                Notification::nSendSms($val->mobile, $msg);
+        
             Notification::nSmsfetion(Yii::app()->params->fetionAccount, $msg);
         }
     }
