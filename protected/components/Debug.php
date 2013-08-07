c3a1706458c2bdc57c458190890cf3d2ad8227f2
@nerrsoft <leon@nerrsoft.com>
2013-08-07 12:16:59 +0800

    use print_r to display array

--- protected/components/Debug.php
+++ protected/components/Debug.php
@@ -2,10 +2,10 @@
 
 class Debug
 {
-	public static function dump($data)
-	{
-		echo '<pre>';
-		var_dump($data);
-		echo '</pre>';
-	}
+    public static function dump($data)
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
 }
