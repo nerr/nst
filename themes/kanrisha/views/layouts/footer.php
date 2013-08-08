600354222f2bb1cff39335c041665910ac49ee1b
@nerrsoft <leon@nerrsoft.com>
2013-08-08 14:04:13 +0800

    add google analytics code

--- themes/kanrisha/views/layouts/footer.php
+++ themes/kanrisha/views/layouts/footer.php
@@ -1,10 +1,19 @@
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	<footer>
-		<div class="wrapper">
-			<span class="copyright">
-				Copyright &copy; <?php echo date('Y'); ?> Nerrsoft.com All rights reserved.
-			</span>
-		</div>
-	</footer><!-- footer -->
+        ga('create', 'UA-28003762-2', 'nerrsoft.com');
+        ga('send', 'pageview');
+
+    </script>
+    <footer>
+        <div class="wrapper">
+            <span class="copyright">
+                Copyright &copy; <?php echo date('Y'); ?> Nerrsoft.com All rights reserved.
+            </span>
+        </div>
+    </footer><!-- footer -->
 </body>
 </html>
