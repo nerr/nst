250b2d936eb141bf58810bf93fdb7e326cc49535
@nerrsoft <leon@nerrsoft.com>
2013-09-13 12:20:44 +0800

    remove <img> html tag

--- protected/components/Tools.php
+++ protected/components/Tools.php
@@ -62,10 +62,10 @@ class Tools
         $url .= md5( strtolower( trim( $email ) ) );
         $url .= "?s=$s&d=$d&r=$r";
         if ( $img ) {
-            $url = '<img src="' . $url . '"';
+            //$url = '<img src="' . $url . '"';
             foreach ( $atts as $key => $val )
                 $url .= ' ' . $key . '="' . $val . '"';
-            $url .= ' />';
+            //$url .= ' />';
         }
         return $url;
     }
