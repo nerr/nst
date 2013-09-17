396524aa54d0235cfeb4d357eeffa4a3fb9b1fe9
@nerrsoft <leon@nerrsoft.com>
2013-09-17 16:21:16 +0800

    update funds view for ace theme

--- protected/views/index/funds.php
+++ protected/views/index/funds.php
@@ -1,53 +1,218 @@
-	<div class="wrapper contents_wrapper">
-		
-		<aside class="sidebar">
-			<ul class="tab_nav">
-				<?php echo $menu; ?>
-			</ul>
-		</aside>
+<?php 
+    /* @var $this Controller */ 
+    $csspath = Yii::app()->request->baseUrl."/themes/ace/css/";
+    $jspath  = Yii::app()->request->baseUrl."/themes/ace/js/";
+    $imgpath = Yii::app()->request->baseUrl."/themes/ace/img/";
+?>
+        <div class="main-container" id="main-container">
+            <script type="text/javascript">
+                try{ace.settings.check('main-container' , 'fixed')}catch(e){}
+            </script>
 
-		<div class="contents">
-			<div class="grid_wrapper">
+            <div class="main-container-inner">
+                <a class="menu-toggler" id="menu-toggler" href="#">
+                    <span class="menu-text"></span>
+                </a>
 
-				<div class="g_6 contents_header">
-					<h3 class="i_16_forms tab_label"><?php echo Yii::t('common', 'Funds'); ?></h3>
-					<div><span class="label_sp"><?php echo Yii::t('common', 'Funds options log'); ?></span></div>
-				</div>
-				<div class="g_6 contents_options">
-					<div class="simple_buttons">
-						<div class="bwIcon i_16_help"><?php echo Yii::t('common', 'Help'); ?></div>
-					</div>
-				</div>
+                <div class="sidebar" id="sidebar"> <!-- nav-list begin -->
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+                    </script>
 
-				<!-- Detail Table -->
-				<div class="g_12">
-					<div class="widget_header">
-						<h4 class="widget_header_title wwIcon i_16_tables"><?php echo Yii::t('common', 'List'); ?></h4>
-					</div>
-					<div class="widget_contents noPadding">
-						<table class="tables">
-							<thead>
-								<tr>
-									<th><?php echo Yii::t('common', 'Option Time'); ?></th>
-									<th><?php echo Yii::t('common', 'Direction'); ?></th>
-									<th><?php echo Yii::t('common', 'Amount'); ?></th>
-									<th><?php echo Yii::t('common', 'Description'); ?></th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php foreach($table as $v){ ?>
-								<tr>
-									<td><?php echo $v->flowtime; ?></td>
-									<td><?php echo Yii::t('common', $v->directioinname); ?></td>
-									<td><?php echo $v->amount; ?></td>
-									<td><?php echo $v->memo; ?></td>
-								</tr>
-								<?php } ?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>		
-		</div>
+                    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+                        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+                            <button class="btn btn-success">
+                                <i class="icon-signal"></i>
+                            </button>
 
-	</div>
+                            <button class="btn btn-info">
+                                <i class="icon-pencil"></i>
+                            </button>
+
+                            <button class="btn btn-warning">
+                                <i class="icon-group"></i>
+                            </button>
+
+                            <button class="btn btn-danger">
+                                <i class="icon-cogs"></i>
+                            </button>
+                        </div>
+
+                        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+                            <span class="btn btn-success"></span>
+
+                            <span class="btn btn-info"></span>
+
+                            <span class="btn btn-warning"></span>
+
+                            <span class="btn btn-danger"></span>
+                        </div>
+                    </div><!-- #sidebar-shortcuts -->
+
+                    <?php echo $menu['html']; ?>
+                    <!-- /.nav-list -->
+
+                    <div class="sidebar-collapse" id="sidebar-collapse">
+                        <i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
+                    </div>
+
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+                    </script>
+                </div>
+
+                <div class="main-content">
+                    <div class="breadcrumbs" id="breadcrumbs">
+                        <script type="text/javascript">
+                            try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+                        </script>
+
+                        <ul class="breadcrumb">
+                            <li>
+                                <i class="icon-home home-icon"></i>
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="active"><?php echo $menu['info']['name']; ?></li>
+                        </ul><!-- .breadcrumb -->
+
+                        <div class="nav-search" id="nav-search">
+                            <form class="form-search">
+                                <span class="input-icon">
+                                    <input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+                                    <i class="icon-search nav-search-icon"></i>
+                                </span>
+                            </form>
+                        </div><!-- #nav-search -->
+                    </div>
+
+                    <div class="page-content">
+                        <div class="page-header">
+                            <h1>
+                                <?php echo $menu['info']['name']; ?>
+                                <small>
+                                    <i class="icon-double-angle-right"></i>
+                                    <?php echo $menu['info']['desc']; ?>
+                                </small>
+                            </h1>
+                        </div><!-- /.page-header -->
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <!-- PAGE CONTENT BEGINS -->
+
+                                <div class="row">
+                                    <div class="col-xs-8">
+
+                                        <div class="table-header">
+                                            Results for P/L detail
+                                        </div>
+
+                                        <div class="table-responsive">
+                                            <table id="sample-table-2" class="table table-striped table-bordered table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th><?php echo Yii::t('common', 'Option Time'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Direction'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Amount'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Description'); ?></th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <?php foreach($table as $v){ ?>
+                                                    <tr>
+                                                        <td><?php echo $v->flowtime; ?></td>
+                                                        <td><?php echo Yii::t('common', $v->directioinname); ?></td>
+                                                        <td><?php echo $v->amount; ?></td>
+                                                        <td><?php echo $v->memo; ?></td>
+                                                    </tr>
+                                                    <?php } ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+
+                        </div><!-- /.row -->
+                    </div><!-- /.page-content -->
+                </div><!-- /.main-content -->
+
+
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<![endif]-->
+
+        <!--[if !IE]> -->
+
+        <script type="text/javascript">
+            window.jQuery || document.write("<script src='<?php echo $jspath; ?>jquery-2.0.3.min.js'>"+"<"+"/script>");
+        </script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script type="text/javascript">
+ window.jQuery || document.write("<script src='<?php echo $jspath; ?>jquery-1.10.2.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+
+        <script type="text/javascript">
+            if("ontouchend" in document) document.write("<script src='<?php echo $jspath; ?>jquery.mobile.custom.min.js'>"+"<"+"/script>");
+        </script>
+        <script src="<?php echo $jspath; ?>bootstrap.min.js"></script>
+        <script src="<?php echo $jspath; ?>typeahead-bs2.min.js"></script>
+
+        <!-- page specific plugin scripts -->
+
+        <script src="<?php echo $jspath; ?>jquery.dataTables.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.dataTables.bootstrap.js"></script>
+
+        <!-- ace scripts -->
+
+        <script src="<?php echo $jspath; ?>ace-elements.min.js"></script>
+        <script src="<?php echo $jspath; ?>ace.min.js"></script>
+
+        <!-- inline scripts related to this page -->
+
+        <script type="text/javascript">
+            jQuery(function($) {
+                var oTable1 = $('#sample-table-2').dataTable( {
+                    "aaSorting": [[ 5, "desc" ]]
+                /*"aoColumns": [
+                  { "bSortable": false },
+                  null, null,null, null, null,
+                  { "bSortable": false }
+                ] */} );
+                
+                
+                $('table th input:checkbox').on('click' , function(){
+                    var that = this;
+                    $(this).closest('table').find('tr > td:first-child input:checkbox')
+                    .each(function(){
+                        this.checked = that.checked;
+                        $(this).closest('tr').toggleClass('selected');
+                    });
+                        
+                });
+            
+            
+                $('[data-rel="tooltip"]').tooltip({placement: tooltip_placement});
+                function tooltip_placement(context, source) {
+                    var $source = $(source);
+                    var $parent = $source.closest('table')
+                    var off1 = $parent.offset();
+                    var w1 = $parent.width();
+            
+                    var off2 = $source.offset();
+                    var w2 = $source.width();
+            
+                    if( parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2) ) return 'right';
+                    return 'left';
+                }
+            })
+        </script>
