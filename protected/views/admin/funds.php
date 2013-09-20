f6d769fbe9c930e2dec07f3f923ced1ebd71fbe0
Leon Zhuang <leon@nerrsoft.com>
2013-09-20 09:47:07 +0800

    finished funds view

--- protected/views/admin/funds.php
+++ protected/views/admin/funds.php
@@ -1,89 +1,248 @@
-    <div class="wrapper contents_wrapper">
-        
-        <aside class="sidebar">
-            <ul class="tab_nav">
-                <?php echo $menu; ?>
-            </ul>
-        </aside>
+<?php 
+    /* @var $this Controller */ 
+    $csspath = Yii::app()->request->baseUrl."/themes/ace/css/";
+    $jspath  = Yii::app()->request->baseUrl."/themes/ace/js/";
+    $imgpath = Yii::app()->request->baseUrl."/themes/ace/img/";
+?>
+        <div class="main-container" id="main-container">
+            <script type="text/javascript">
+                try{ace.settings.check('main-container' , 'fixed')}catch(e){}
+            </script>
 
-        <div class="contents">
-            <div class="grid_wrapper">
+            <div class="main-container-inner">
+                <a class="menu-toggler" id="menu-toggler" href="#">
+                    <span class="menu-text"></span>
+                </a>
 
-                <div class="g_6 contents_header">
-                    <h3 class="i_16_forms tab_label"><?php echo Yii::t('common', 'Funds'); ?></h3>
-                    <div><span class="label_sp"><?php echo Yii::t('common', 'Funds Flow log'); ?></span></div>
+                <div class="sidebar" id="sidebar"> <!-- nav-list begin -->
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+                    </script>
+
+                    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+                        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+                            <button class="btn btn-success">
+                                <i class="icon-signal"></i>
+                            </button>
+
+                            <button class="btn btn-info">
+                                <i class="icon-pencil"></i>
+                            </button>
+
+                            <button class="btn btn-warning">
+                                <i class="icon-group"></i>
+                            </button>
+
+                            <button class="btn btn-danger">
+                                <i class="icon-cogs"></i>
+                            </button>
+                        </div>
+
+                        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+                            <span class="btn btn-success"></span>
+
+                            <span class="btn btn-info"></span>
+
+                            <span class="btn btn-warning"></span>
+
+                            <span class="btn btn-danger"></span>
+                        </div>
+                    </div><!-- #sidebar-shortcuts -->
+
+                    <?php echo $menu['html']; ?>
+                    <!-- /.nav-list -->
+
+                    <div class="sidebar-collapse" id="sidebar-collapse">
+                        <i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
+                    </div>
+
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+                    </script>
                 </div>
-                <div class="g_6 contents_options">
-                    <div class="simple_buttons">
-                        <div class="bwIcon i_16_help"><?php echo Yii::t('common', 'Excel'); ?></div>
-                    </div>
-                </div>
 
-                <!-- Summary Table -->
-                <div class="g_12">
-                    <div class="widget_header">
-                        <h4 class="widget_header_title wwIcon i_16_tables"><?php echo Yii::t('common', 'Summary'); ?></h4>
-                    </div>
-                    <div class="widget_contents noPadding">
-                        <table class="tables">
-                            <thead>
-                                <tr>
-                                    <th><?php echo Yii::t('common', 'Deposit'); ?></th>
-                                    <th><?php echo Yii::t('common', 'Withdraw'); ?></th>
-                                    <th><?php echo Yii::t('common', 'Commission'); ?></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><a href="<?php echo $url['funds'];?>"><?php echo number_format($summary['deposit'],2); ?></a></td>
-                                    <td><?php echo number_format($summary['withdraw'],2); ?></td>
-                                    <td><?php echo number_format($summary['commission'],2); ?></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <div class="main-content">
+                    <div class="breadcrumbs" id="breadcrumbs">
+                        <script type="text/javascript">
+                            try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+                        </script>
 
-                <!-- Detail Table -->
-                <div class="g_12">
-                    <div class="widget_header">
-                        <h4 class="widget_header_title wwIcon i_16_tables"><?php echo Yii::t('common', 'List'); ?></h4>
-                    </div>
-                    <div class="widget_contents noPadding">
-                        <table class="tables">
-                            <thead>
-                                <tr>
-                                    <th><?php echo Yii::t('common', 'Users'); ?></th>
-                                    <th><?php echo Yii::t('common', 'Time'); ?></th>
-                                    <th><?php echo Yii::t('common', 'Direction'); ?></th>
-                                    <th><?php echo Yii::t('common', 'Amount'); ?></th>
-                                    <th><?php echo Yii::t('common', 'Memo'); ?></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                foreach($detail as $key=>$val){
-                                    foreach($val['row'] as $v){
-                                ?>
-                                <tr>
-                                    <td><?php echo $key; ?></td>
-                                    <td><?php echo $v['time']; ?></td>
-                                    <td><?php echo Yii::t('common', $v['direction']); ?></td>
-                                    <td><?php echo number_format($v['amount'], 2); ?></td>
-                                    <td><?php echo $v['memo']; ?></td>
-                                </tr>
-                                <?php } ?>
-                                <tr style="background-color:#EEF3F6">
-                                    <td colspan="3">&nbsp;</td>
-                                    <td><b><?php echo number_format($val['total'], 2); ?></b></td>
-                                    <td>&nbsp;</td>
-                                </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>      
-        </div>
+                        <ul class="breadcrumb">
+                            <li>
+                                <i class="icon-home home-icon"></i>
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="active"><?php echo $menu['info']['name']; ?></li>
+                        </ul><!-- .breadcrumb -->
 
-    </div>
+                        <div class="nav-search" id="nav-search">
+                            <form class="form-search">
+                                <span class="input-icon">
+                                    <input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+                                    <i class="icon-search nav-search-icon"></i>
+                                </span>
+                            </form>
+                        </div><!-- #nav-search -->
+                    </div>
+
+                    <div class="page-content">
+                        <div class="page-header">
+                            <h1>
+                                <?php echo $menu['info']['name']; ?>
+                                <small>
+                                    <i class="icon-double-angle-right"></i>
+                                    <?php echo $menu['info']['desc']; ?>
+                                </small>
+                            </h1>
+                        </div><!-- /.page-header -->
+
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <div class="widget-box">
+                                    <div class="widget-header widget-header-flat widget-header-small">
+                                        <h5>
+                                            <i class="icon-signal"></i>
+                                            Percentage of funds
+                                        </h5>
+                                    </div>
+
+                                    <div class="widget-body">
+                                        <div class="widget-main">
+                                            <div id="piechart-placeholder"></div>
+                                        </div><!-- /widget-main -->
+                                    </div><!-- /widget-body -->
+                                </div><!-- /widget-box -->
+                            </div><!-- /span -->
+                        </div><!-- /pie chart -->
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <!-- PAGE CONTENT BEGINS -->
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <div class="table-responsive">
+                                            <h4 class="lighter">
+                                                <i class="icon-list"></i>
+                                                Desposit &amp; Withdraw detail
+                                            </h4>
+                                            <table id="sample-table-1" class="table table-striped table-bordered table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th><?php echo Yii::t('common', 'Users'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Time'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Direction'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Amount'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Memo'); ?></th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <?php 
+                                                    foreach($detail as $key=>$val){
+                                                        foreach($val['row'] as $v){
+                                                    ?>
+                                                    <tr>
+                                                        <td><?php echo $key; ?></td>
+                                                        <td><?php echo $v['time']; ?></td>
+                                                        <td><?php echo Yii::t('common', $v['direction']); ?></td>
+                                                        <td><?php echo number_format($v['amount'], 2); ?></td>
+                                                        <td><?php echo $v['memo']; ?></td>
+                                                    </tr>
+                                                    <?php } ?>
+                                                    <tr>
+                                                        <th colspan="3" class="center"><b>Summary</b></th>
+                                                        <th colspan="2" class="center"><b><?php echo number_format($val['total'], 2); ?></b></th>
+                                                    </tr>
+                                                    <?php } ?>
+                                                </tbody>
+                                            </table>
+                                        </div><!-- /.table-responsive -->
+                                    </div><!-- /span -->
+                                </div><!-- /row.summary table -->
+
+                        </div><!-- /.row -->
+                    </div><!-- /.page-content -->
+                </div><!-- /.main-content -->
+
+
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<![endif]-->
+
+        <!--[if !IE]> -->
+
+        <script type="text/javascript">
+            window.jQuery || document.write("<script src='<?php echo $jspath; ?>jquery-2.0.3.min.js'>"+"<"+"/script>");
+        </script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script type="text/javascript">
+ window.jQuery || document.write("<script src='<?php echo $jspath; ?>jquery-1.10.2.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+
+        <script type="text/javascript">
+            if("ontouchend" in document) document.write("<script src='<?php echo $jspath; ?>jquery.mobile.custom.min.js'>"+"<"+"/script>");
+        </script>
+        <script src="<?php echo $jspath; ?>bootstrap.min.js"></script>
+        <script src="<?php echo $jspath; ?>typeahead-bs2.min.js"></script>
+
+        <!-- page specific plugin scripts -->
+
+        <script src="<?php echo $jspath; ?>jquery.dataTables.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.dataTables.bootstrap.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.easy-pie-chart.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.sparkline.min.js"></script>
+        <script src="<?php echo $jspath; ?>flot/jquery.flot.min.js"></script>
+        <script src="<?php echo $jspath; ?>flot/jquery.flot.pie.min.js"></script>
+        <script src="<?php echo $jspath; ?>flot/jquery.flot.resize.min.js"></script>
+
+        <!-- ace scripts -->
+
+        <script src="<?php echo $jspath; ?>ace-elements.min.js"></script>
+        <script src="<?php echo $jspath; ?>ace.min.js"></script>
+
+        <!-- inline scripts related to this page -->
+
+        <script type="text/javascript">
+            jQuery(function($) {
+                var placeholder = $('#piechart-placeholder').css({'width':'80%' , 'min-height':'205px'});
+                var data = <?php echo $chart; ?>;
+                function drawPieChart(placeholder, data, position) {
+                    $.plot(placeholder, data, {
+                        series: {
+                            pie: {
+                                show: true,
+                                tilt:0.8,
+                                highlight: {
+                                    opacity: 0.25
+                                },
+                                stroke: {
+                                    color: '#fff',
+                                    width: 2
+                                },
+                                startAngle: 2
+                            }
+                        },
+                        legend: {
+                            show: true,
+                            position: position || "ne", 
+                            labelBoxBorderColor: null,
+                            margin:[-30,15]
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true
+                        }
+                    })
+                }
+                drawPieChart(placeholder, data);
+            })
+        </script>
