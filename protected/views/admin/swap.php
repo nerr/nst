a0a2da4bcb3dee45ed4f9b74b101fcdcf3e8f4e3
Leon Zhuang <leon@nerrsoft.com>
2013-09-21 13:57:24 +0800

    update admin swap view for ace theme

--- protected/views/admin/swap.php
+++ protected/views/admin/swap.php
@@ -1,115 +1,269 @@
-	<div class="wrapper contents_wrapper">
-		
-		<aside class="sidebar">
-			<ul class="tab_nav">
-				<?php echo $menu; ?>
-			</ul>
-		</aside>
+<?php 
+    /* @var $this Controller */ 
+    $csspath = Yii::app()->request->baseUrl."/themes/ace/css/";
+    $jspath  = Yii::app()->request->baseUrl."/themes/ace/js/";
+    $imgpath = Yii::app()->request->baseUrl."/themes/ace/img/";
+?>
+        <div class="main-container" id="main-container">
+            <script type="text/javascript">
+                try{ace.settings.check('main-container' , 'fixed')}catch(e){}
+            </script>
 
-		<div class="contents">
-			<div class="grid_wrapper">
+            <div class="main-container-inner">
+                <a class="menu-toggler" id="menu-toggler" href="#">
+                    <span class="menu-text"></span>
+                </a>
 
-				<div class="g_6 contents_header">
-					<h3 class="i_16_dashboard tab_label"><?php echo Yii::t('common', 'Swap'); ?></h3>
-					<div><span class="label_sp"><?php echo Yii::t('common', 'Swap Rate Chart'); ?></span></div>
-				</div>
-				<div class="g_6 contents_options">
-					<div class="simple_buttons">
-						<div class="bwIcon i_16_help"><?php echo Yii::t('common', 'Help'); ?></div>
-					</div>
-				</div>
+                <div class="sidebar" id="sidebar"> <!-- nav-list begin -->
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+                    </script>
 
-				<div class="g_12 separator"><span></span></div>
+                    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+                        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+                            <button class="btn btn-success">
+                                <i class="icon-signal"></i>
+                            </button>
 
-				<!-- Quick Statistics -->
-                <div class="g_3 quick_stats">
-                    <div class="big_stats visitor_stats">
-                        <?php echo number_format($swapavg['EURMXN']['short'],2); ?>
+                            <button class="btn btn-info">
+                                <i class="icon-pencil"></i>
+                            </button>
+
+                            <button class="btn btn-warning">
+                                <i class="icon-group"></i>
+                            </button>
+
+                            <button class="btn btn-danger">
+                                <i class="icon-cogs"></i>
+                            </button>
+                        </div>
+
+                        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+                            <span class="btn btn-success"></span>
+
+                            <span class="btn btn-info"></span>
+
+                            <span class="btn btn-warning"></span>
+
+                            <span class="btn btn-danger"></span>
+                        </div>
+                    </div><!-- #sidebar-shortcuts -->
+
+                    <?php echo $menu['html']; ?>
+                    <!-- /.nav-list -->
+
+                    <div class="sidebar-collapse" id="sidebar-collapse">
+                        <i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
                     </div>
-                    <h5 class="stats_info"><?php echo Yii::t('common', 'SymbolA Avg'); ?></h5>
+
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+                    </script>
                 </div>
-                <div class="g_3 quick_stats">
-                    <div class="big_stats orders_stats">
-                        <?php echo number_format($swapavg['USDMXN']['short'],2); ?>
+
+                <div class="main-content">
+                    <div class="breadcrumbs" id="breadcrumbs">
+                        <script type="text/javascript">
+                            try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+                        </script>
+
+                        <ul class="breadcrumb">
+                            <li>
+                                <i class="icon-home home-icon"></i>
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="active"><?php echo $menu['info']['name']; ?></li>
+                        </ul><!-- .breadcrumb -->
+
+                        <div class="nav-search" id="nav-search">
+                            <form class="form-search">
+                                <span class="input-icon">
+                                    <input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+                                    <i class="icon-search nav-search-icon"></i>
+                                </span>
+                            </form>
+                        </div><!-- #nav-search -->
                     </div>
-                    <h5 class="stats_info"><?php echo Yii::t('common', 'SymbolB Avg'); ?></h5>
-                </div>
 
-                <div class="g_12 separator"><span></span></div>
+                    <div class="page-content">
+                        <div class="page-header">
+                            <h1>
+                                <?php echo $menu['info']['name']; ?>
+                                <small>
+                                    <i class="icon-double-angle-right"></i>
+                                    <?php echo $menu['info']['desc']; ?>
+                                </small>
+                            </h1>
+                        </div><!-- /.page-header -->
 
-				<!-- swap rate Charts -->
-                <div class="g_12">
-                    <div class="widget_header">
-                        <h4 class="widget_header_title wwIcon i_16_charts"><?php echo Yii::t('common', 'Swap Rate Chart'); ?></h4>
-                    </div>
-                    <div class="widget_contents">
-                        <div class="swaprate"></div>
-                    </div>
-                </div>
-			</div>
-		</div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <!-- PAGE CONTENT BEGINS -->
 
-	</div>
+                                <div class="row">
+                                    <div class="space-6"></div>
 
-	<script type="text/javascript">
-	if (!!$(".swaprate").offset())
-    {
-        var symbol_1 = <?php echo $swapratechart['EURMXNshort']; ?>;
-        var symbol_2 = <?php echo $swapratechart['USDMXNshort']; ?>;
-        var symbol_3 = <?php echo $swapratechart['MXNJPYshort']; ?>;
-        var symbol_4 = <?php echo $swapratechart['USDJPYlong']; ?>;
-        var symbol_5 = <?php echo $swapratechart['EURJPYlong']; ?>;
+                                    <div class="col-sm-7">
+                                        <!-- principal balance -->
+                                        <div class="infobox infobox-orange  ">
+                                            <div class="infobox-icon">
+                                                <i class="icon-money"></i>
+                                            </div>
 
-        for(i = 0; i < symbol_1.length; i++)
-        {
-            symbol_1[i][0] *= 1000;
-            symbol_2[i][0] *= 1000;
-            symbol_3[i][0] *= 1000;
-            symbol_4[i][0] *= 1000;
-            symbol_5[i][0] *= 1000;
-        }
+                                            <div class="infobox-data">
+                                                <span class="infobox-data-number"><?php echo number_format($swapavg['EURMXN']['short'],2); ?></span>
+                                                <div class="infobox-content">SymbolA Avg</div>
+                                            </div>
+                                        </div>
 
-        //
-        $.plot($(".swaprate"),[ 
-            { label: "<?php echo Yii::t('common', 'SymbolA'); ?>", data: symbol_1 }, 
-            { label: "<?php echo Yii::t('common', 'SymbolB'); ?>", data: symbol_2 },
-            { label: "<?php echo Yii::t('common', 'SymbolC'); ?>", data: symbol_3 },
-            { label: "<?php echo Yii::t('common', 'SymbolD'); ?>", data: symbol_4 },
-            { label: "<?php echo Yii::t('common', 'SymbolE'); ?>", data: symbol_5 }
-        ],{
-            colors: ["#cc3333", "#00AADD", "#cccc33", "#FF0055", "#00FF00"],
+                                        <!-- account balance -->
+                                        <div class="infobox infobox-blue  ">
+                                            <div class="infobox-icon">
+                                                <i class="icon-money"></i>
+                                            </div>
 
-            series: {
-                lines: {
+                                            <div class="infobox-data">
+                                                <span class="infobox-data-number"><?php echo number_format($swapavg['USDMXN']['short'],2); ?></span>
+                                                <div class="infobox-content">SymbolB Avg</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="hr hr32 hr-dotted"></div>
+
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <h4 class="lighter">
+                                            <i class="icon-signal"></i>
+                                            Swap Rate Chart
+                                        </h4>
+                                        <div id="swap-rate-chart"></div>
+                                    </div>
+                                </div>
+
+                                <!-- PAGE CONTENT ENDS -->
+                            </div><!-- /.col -->
+                        </div><!-- /.row -->
+                    </div><!-- /.page-content -->
+                </div><!-- /.main-content -->
+            </div><!-- /.main-container-inner -->
+
+            <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+                <i class="icon-double-angle-up icon-only bigger-110"></i>
+            </a>
+        </div><!-- /.main-container -->
+
+        <!-- basic scripts -->
+
+        <!--[if !IE]> -->
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<![endif]-->
+
+        <!--[if !IE]> -->
+
+        <script type="text/javascript">
+            window.jQuery || document.write("<script src='<?php echo $jspath; ?>jquery-2.0.3.min.js'>"+"<"+"/script>");
+        </script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script type="text/javascript">
+ window.jQuery || document.write("<script src='js/jquery-1.10.2.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+
+        <script type="text/javascript">
+            if("ontouchend" in document) document.write("<script src='<?php echo $jspath; ?>jquery.mobile.custom.min.js'>"+"<"+"/script>");
+        </script>
+        <script src="<?php echo $jspath; ?>bootstrap.min.js"></script>
+        <script src="<?php echo $jspath; ?>typeahead-bs2.min.js"></script>
+
+        <!-- page specific plugin scripts -->
+
+        <!--[if lte IE 8]>
+          <script src="<?php echo $jspath; ?>excanvas.min.js"></script>
+        <![endif]-->
+
+        <script src="<?php echo $jspath; ?>jquery-ui-1.10.3.custom.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.ui.touch-punch.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.slimscroll.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.easy-pie-chart.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.sparkline.min.js"></script>
+        <script src="<?php echo $jspath; ?>flot/jquery.flot.min.js"></script>
+        <script src="<?php echo $jspath; ?>flot/jquery.flot.pie.min.js"></script>
+        <script src="<?php echo $jspath; ?>flot/jquery.flot.resize.min.js"></script>
+
+        <!-- ace scripts -->
+
+        <script src="<?php echo $jspath; ?>ace-elements.min.js"></script>
+        <script src="<?php echo $jspath; ?>ace.min.js"></script>
+
+        <!-- inline scripts related to this page -->
+
+        <script type="text/javascript">
+            jQuery(function($) {
+                //-- line chart begin
+                var d1 = <?php echo $swapratechart['EURMXNshort']; ?>;
+                var d2 = <?php echo $swapratechart['USDMXNshort']; ?>;
+                var d3 = <?php echo $swapratechart['MXNJPYshort']; ?>;
+                var d4 = <?php echo $swapratechart['USDJPYlong']; ?>;
+                var d5 = <?php echo $swapratechart['EURJPYlong']; ?>;
+
+                for(var i = 0; i < d1.length; i++)
+                {
+                    d1[i][0] = (new Date(d1[i][0])).getTime();
+                    d2[i][0] = (new Date(d2[i][0])).getTime();
+                    d3[i][0] = (new Date(d3[i][0])).getTime();
+                    d4[i][0] = (new Date(d4[i][0])).getTime();
+                    d5[i][0] = (new Date(d5[i][0])).getTime();
+                }
+            
+                var sales_charts = $('#swap-rate-chart').css({'width':'100%' , 'height':'420px'});
+                $.plot("#swap-rate-chart", [
+                    { label: "SymbolA", data: d1 },
+                    { label: "SymbolB", data: d2 },
+                    { label: "SymbolC", data: d3 },
+                    { label: "SymbolD", data: d4 },
+                    { label: "SymbolE", data: d5 }
+                ], {
+                    hoverable: true,
+                    shadowSize: 0,
+                    colors: ["#cccc33", "#00AADD", "#cc3333"],
+                    series: {
+                        lines: {
+                                show: true,
+                                lineWidth: 2
+                               },
+                        points: {show: false},
+                        shadowSize: 2
+                    },
+                    grid: {
+                        backgroundColor: { colors: [ "#fff", "#fff" ] },
+                        hoverable: true,
                         show: true,
-                        lineWidth: 2
-                       },
-                points: {show: true},
-                shadowSize: 2
-            },
-
-            grid: {
-                hoverable: true,
-                show: true,
-                borderWidth: 0,
-                tickColor: "#d2d2d2",
-                labelMargin: 12
-            },
-
-            legend: {
-                show: true,
-                margin: [0,-24],
-                noColumns: 0,
-                labelBoxBorderColor: null
-            },
-
-            yaxis: {},
-
-            xaxis: {
-                mode:"time", 
-                timeformat: "%m-%d", 
-                minTickSize: [1, "day"]
-            }
-        });
-    }
-    </script>
+                        borderWidth: 0,
+                        tickColor: "#d2d2d2",
+                        labelMargin: 12
+                    },
+                    legend: {
+                        show: true,
+                        noColumns: 0,
+                    },
+                    xaxis: {
+                        //mode: "time",
+                        /*timeformat: "%m-%d", 
+                        minTickSize: [3, "day"]*/
+                    },
+                    yaxis: {}
+                });//-- line chart end
+                
+            })
+        </script>
