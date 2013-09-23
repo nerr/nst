6c94fe12d1b1bf1af657b96afa65917a9b79fdfd
@nerrsoft <leon@nerrsoft.com>
2013-09-23 14:54:22 +0800

    finished admin pl report view

--- protected/views/admin/report.php
+++ protected/views/admin/report.php
@@ -1,87 +1,207 @@
-	<div class="wrapper contents_wrapper">
-		
-		<aside class="sidebar">
-			<ul class="tab_nav">
-				<?php echo $menu; ?>
-			</ul>
-		</aside>
+<?php 
+    /* @var $this Controller */ 
+    $csspath = Yii::app()->request->baseUrl."/themes/ace/css/";
+    $jspath  = Yii::app()->request->baseUrl."/themes/ace/js/";
+    $imgpath = Yii::app()->request->baseUrl."/themes/ace/img/";
+?>
+        <div class="main-container" id="main-container">
+            <script type="text/javascript">
+                try{ace.settings.check('main-container' , 'fixed')}catch(e){}
+            </script>
 
-		<div class="contents">
-			<div class="grid_wrapper">
+            <div class="main-container-inner">
+                <a class="menu-toggler" id="menu-toggler" href="#">
+                    <span class="menu-text"></span>
+                </a>
 
-				<div class="g_6 contents_header">
-					<h3 class="i_16_table tab_label"><?php echo Yii::t('common', 'Report'); ?></h3>
-					<div><span class="label_sp"><?php echo Yii::t('common', 'Report of daily P/L'); ?></span></div>
-				</div>
-				<div class="g_6 contents_options">
-					<div class="simple_buttons">
-						<a href="<?php echo $url['excel'];?>" target="_blank">
-						<div class="bwIcon i_16_help"><?php echo Yii::t('common', 'Export Excel'); ?></div></a>
-					</div>
-				</div>
+                <div class="sidebar" id="sidebar"> <!-- nav-list begin -->
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+                    </script>
 
-				<div class="g_12 separator"><span></span></div>
+                    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+                        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+                            <button class="btn btn-success">
+                                <i class="icon-signal"></i>
+                            </button>
 
-				<!-- Summary Table -->
-				<div class="g_12">
-					<div class="widget_header">
-						<h4 class="widget_header_title wwIcon i_16_tables"><?php echo Yii::t('common', 'Summary'); ?></h4>
-					</div>
-					<div class="widget_contents noPadding">
-						<table class="tables">
-							<thead>
-								<tr>
-									<th><?php echo Yii::t('common', 'Capital'); ?></th>
-									<th><?php echo Yii::t('common', 'Yield'); ?></th>
-									<th><?php echo Yii::t('common', 'Yield Rate'); ?></th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td><a href="<?php echo $url['funds'];?>"><?php echo $summary['capital']; ?></a></td>
-									<td><?php echo $summary['yield']; ?></td>
-									<td><?php echo $summary['yieldrate']; ?>%</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
+                            <button class="btn btn-info">
+                                <i class="icon-pencil"></i>
+                            </button>
 
-				<!-- Detail Table -->
-				<div class="g_12">
-					<div class="widget_header">
-						<h4 class="widget_header_title wwIcon i_16_tables"><?php echo Yii::t('common', 'Detail'); ?></h4>
-					</div>
-					<div class="widget_contents noPadding">
-						<table class="datatable tables">
-							<thead>
-								<tr>
-									<th><?php echo Yii::t('common', 'Date'); ?></th>
-									<th><?php echo Yii::t('common', 'New SWAP'); ?></th>
-									<th><?php echo Yii::t('common', 'Total SWAP'); ?></th>
-									<th><?php echo Yii::t('common', 'Total P/L'); ?></th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php 
-								if(count($detail) > 0){
-								foreach(array_reverse($detail) as $key=>$val){ ?>
-								<tr>
-									<td><?php echo $key.' ('.date('D', strtotime($key)).')'; ?></td>
-									<td><?php echo $val['newswap']; ?></td>
-									<td><?php echo $val['totalswap']; ?></td>
-									<td><?php echo $val['totalpl']; ?></td>
-								</tr>
-								<?php } } ?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>		
-		</div>
+                            <button class="btn btn-warning">
+                                <i class="icon-group"></i>
+                            </button>
 
-	</div>
+                            <button class="btn btn-danger">
+                                <i class="icon-cogs"></i>
+                            </button>
+                        </div>
 
-	<script type="text/javascript">
-	
-	</script>
+                        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+                            <span class="btn btn-success"></span>
+
+                            <span class="btn btn-info"></span>
+
+                            <span class="btn btn-warning"></span>
+
+                            <span class="btn btn-danger"></span>
+                        </div>
+                    </div><!-- #sidebar-shortcuts -->
+
+                    <?php echo $menu['html']; ?>
+                    <!-- /.nav-list -->
+
+                    <div class="sidebar-collapse" id="sidebar-collapse">
+                        <i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
+                    </div>
+
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+                    </script>
+                </div>
+
+                <div class="main-content">
+                    <div class="breadcrumbs" id="breadcrumbs">
+                        <script type="text/javascript">
+                            try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+                        </script>
+
+                        <ul class="breadcrumb">
+                            <li>
+                                <i class="icon-home home-icon"></i>
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="active"><?php echo $menu['info']['name']; ?></li>
+                        </ul><!-- .breadcrumb -->
+
+                        <div class="nav-search" id="nav-search">
+                            <form class="form-search">
+                                <span class="input-icon">
+                                    <input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+                                    <i class="icon-search nav-search-icon"></i>
+                                </span>
+                            </form>
+                        </div><!-- #nav-search -->
+                    </div>
+
+                    <div class="page-content">
+                        <div class="page-header">
+                            <h1>
+                                <?php echo $menu['info']['name']; ?>
+                                <small>
+                                    <i class="icon-double-angle-right"></i>
+                                    <?php echo $menu['info']['desc']; ?>
+                                </small>
+                            </h1>
+                        </div><!-- /.page-header -->
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <!-- PAGE CONTENT BEGINS -->
+
+                                <div class="row">
+                                    <div class="col-xs-8">
+                                        <div class="table-header">
+                                            Results for P/L detail
+                                        </div>
+
+                                        <div class="table-responsive">
+                                            <table id="pl-table" class="table table-striped table-bordered table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th><?php echo Yii::t('common', 'Date'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'New SWAP'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Total SWAP'); ?></th>
+                                                        <th><?php echo Yii::t('common', 'Total P/L'); ?></th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <?php 
+                                                    if(count($detail) > 0){
+                                                    foreach(array_reverse($detail) as $key=>$val){ ?>
+                                                    <tr>
+                                                        <td><?php echo $key.' ('.date('D', strtotime($key)).')'; ?></td>
+                                                        <td><?php echo number_format($val['newswap'], 2); ?></td>
+                                                        <td><?php echo number_format($val['totalswap'], 2); ?></td>
+                                                        <td><?php echo number_format($val['totalpl'], 2); ?></td>
+                                                    </tr>
+                                                    <?php } } ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- PAGE CONTENT ENDS -->
+                            </div><!-- /.col -->
+                        </div><!-- /.row -->
+                    </div><!-- /.page-content -->
+                </div><!-- /.main-content -->
+            </div><!-- /.main-container-inner -->
+
+            <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+                <i class="icon-double-angle-up icon-only bigger-110"></i>
+            </a>
+        </div><!-- /.main-container -->
+
+        <!-- basic scripts -->
+
+        <!--[if !IE]> -->
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<![endif]-->
+
+        <!--[if !IE]> -->
+
+        <script type="text/javascript">
+            window.jQuery || document.write("<script src='<?php echo $jspath; ?>jquery-2.0.3.min.js'>"+"<"+"/script>");
+        </script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+<script type="text/javascript">
+ window.jQuery || document.write("<script src='js/jquery-1.10.2.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+
+        <script type="text/javascript">
+            if("ontouchend" in document) document.write("<script src='<?php echo $jspath; ?>jquery.mobile.custom.min.js'>"+"<"+"/script>");
+        </script>
+        <script src="<?php echo $jspath; ?>bootstrap.min.js"></script>
+        <script src="<?php echo $jspath; ?>typeahead-bs2.min.js"></script>
+
+        <!-- page specific plugin scripts -->
+
+        <!--[if lte IE 8]>
+          <script src="<?php echo $jspath; ?>excanvas.min.js"></script>
+        <![endif]-->
+
+        <script src="<?php echo $jspath; ?>jquery.dataTables.min.js"></script>
+        <script src="<?php echo $jspath; ?>jquery.dataTables.bootstrap.js"></script>
+
+        <!-- ace scripts -->
+
+        <script src="<?php echo $jspath; ?>ace-elements.min.js"></script>
+        <script src="<?php echo $jspath; ?>ace.min.js"></script>
+
+        <!-- inline scripts related to this page -->
+
+        <script type="text/javascript">
+            jQuery(function($) {
+                var oTable1 = $('#pl-table').dataTable( {
+                    "aaSorting": [[ 5, "desc" ]]
+                /*"aoColumns": [
+                  { "bSortable": false },
+                  null, null,null, null, null,
+                  { "bSortable": false }
+                ] */} );
+            })
+        </script>
