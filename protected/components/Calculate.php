32ba110b251d16cd6d4c6f981758d11ae78fd12e
Leon Zhuang <leon@nerrsoft.com>
2013-09-25 21:00:46 +0800

    try to use memcached

--- protected/components/Calculate.php
+++ protected/components/Calculate.php
@@ -19,8 +19,16 @@ class Calculate
     {
         //-- init data
         $data = array();
+        /*
+        //-- make cache id
+        $trace = debug_backtrace();
+        $cacheId = $trace[0]["function"].'_'.$uid;
+        //-- check cache
+        $data = Yii::app()->cache->get($cacheId);
+        if(count($data) > 0)
+            return $data;*/
 
-        //--
+        //-- get data if no cache 
         $criteria = new CDbCriteria;
         $criteria->select = 'orderticket,profit,swap,logdatetime,orderstatus,closedate,getswap,endprofit,commission';
         if($uid > 0)
@@ -150,6 +158,7 @@ class Calculate
             }
         }
 
+        // Yii::app()->cache->set($cacheId, $data, 60);
         return $data;
     }
 
