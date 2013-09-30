bc7025a16a8cc586247feb6eabbd4c74d544667f
@nerrsoft <leon@nerrsoft.com>
2013-09-30 19:23:35 +0800

    change text

--- themes/ace/views/default/login.php
+++ themes/ace/views/default/login.php
@@ -112,7 +112,7 @@
                                             <div>
                                                 <a href="#" onclick="show_box('forgot-box'); return false;" class="forgot-password-link">
                                                     <i class="icon-arrow-left"></i>
-                                                    I forgot my password
+                                                    Forgot password
                                                 </a>
                                             </div>
 
