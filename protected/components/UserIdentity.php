13391cd7a1a74f5e5f0467a1fb971238aa81ad8d
@nerrsoft <leon@nerrsoft.com>
2013-09-30 19:22:36 +0800

    replace tab to space

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,31 +7,31 @@
  */
 class UserIdentity extends CUserIdentity
 {
-	private $_id;
+    private $_id;
 
-	public function authenticate()
-	{
-		$record=SysUser::model()->findByAttributes(array('email'=>$this->username));
+    public function authenticate()
+    {
+        $record=SysUser::model()->findByAttributes(array('email'=>$this->username));
 
-		if($record===null)
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($record->password!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-		{
-			$this->_id=$record->id;
-			$this->setState('avatar', Tools::tGetGavatar($record->email));
-			$this->setState('name', $record->username);
-			$this->setState('email', $record->email);
-			$this->setState('gid', $record->usergroupid);
-			$this->errorCode = self::ERROR_NONE;
-		}
+        if($record===null)
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        else if($record->password!==$this->password)
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
+        else
+        {
+            $this->_id=$record->id;
+            $this->setState('avatar', Tools::tGetGavatar($record->email));
+            $this->setState('name', $record->username);
+            $this->setState('email', $record->email);
+            $this->setState('gid', $record->usergroupid);
+            $this->errorCode = self::ERROR_NONE;
+        }
 
-		return !$this->errorCode;
-	}
+        return !$this->errorCode;
+    }
  
-	public function getId()
-	{
-		return $this->_id;
-	}
+    public function getId()
+    {
+        return $this->_id;
+    }
 }
