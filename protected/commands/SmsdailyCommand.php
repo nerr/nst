0bd9323cc83ee3f88a7996e21ee65e4ae33bcc43
@nerrsoft <leon@nerrsoft.com>
2013-10-23 17:31:03 +0800

    fix sms text display bug

--- protected/commands/SmsdailyCommand.php
+++ protected/commands/SmsdailyCommand.php
@@ -17,11 +17,11 @@ class SmsdailyCommand extends CConsoleCommand
             $msg = '截止'.$data['summary']['lastuptodate']."\r\n";
             $msg .= '账户余额: '.number_format($data['summary']['balance'], 2)."\r\n";
             $msg .= '交易成本: '.number_format($data['summary']['cost'], 2)."\r\n";
-            $msg .= '上一交易日获得掉期: '.number_format($weeks[date('N',strtotime($data['summary']['lastuptodate']))]['swap_new'], 2)."\r\n";
+            $msg .= '上一交易日获得掉期：'.number_format($weeks[date('N',strtotime($data['summary']['lastuptodate']))]['swap_new'], 2)."\r\n";
             $msg .= '累计获得掉期: '.number_format($data['summary']['swap'], 2)."\r\n";
             $msg .= '浮动收益: '.number_format($data['summary']['netearning'], 2)."\r\n";
             if(($data['summary']['balance'] - $data['summary']['netearning']) > 0)
-                $msg .= '收益率: '.number_format($data['summary']['netearning']/($data['summary']['balance'] - $data['summary']['netearning'])*100, 2).'%'."\r\n";;
+                $msg .= '收益率：'.number_format($data['summary']['netearning']/($data['summary']['balance'] - $data['summary']['netearning'])*100, 2).'%'."\r\n";;
             $msg .= '[NST自动短信报表]';
 
             //$m = echo mb_convert_encoding($msg, 'GBK', 'UTF-8');
